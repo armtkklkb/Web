9abb0ac7b057795ccf8e7dbd3f418ce4f958c780
armtkklkb <armtkklkb@gmail.com>
2015-12-03 00:33:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <html>
 
+<html>
+<center><head><h1><font color="#FFFFFF">Profile</font></h1></head><center><br>
+<body bgcolor = "gray"><br>
+<center><a href="[url=http://www.mx7.com/view2/yLoJVotWL4aXqcEx][img]http://www.mx7.com/i/405/8LjOe1.jpg[/img][/url]></a>
+<center>
+<center><h2><font color="#FFFFFF">
+  I would like to introduce myself. <br>
+  My name is Theerawat Jongjaroen My nickname Arm . <br> 
+  I was born on September 27,1994 I am 21 years old.<br>
+  I am tird 1st year student at faculty of Seience and Technology.<br> 
+  My major is Information and communication technology at University of the Thai Chamber of Commerce.
+</font></h2>
+
+</center>
+</body>
+</html>
+
 <body>
 5805404010. 
 </body>
